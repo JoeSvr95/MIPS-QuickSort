5477c6029c5630c53d5a074be6a2b87093781d0f
JoeSvr95 <joesaverio@hotmail.com>
2018-06-24 20:50:12 -0500

    Función quickSortIterative

--- proyecto.asm
+++ proyecto.asm
@@ -2,6 +2,7 @@
 	numArray: .word 4, 3, 5, 2, 1, 3, 2, 3 # arr[] = {4, 3, 5, 2, 1, 3, 2, 3}
 	size: .word 8
 	indice: .word 0
+	stack: .word 0:size
 	coma: .asciiz ", "
 .text
 	main:
@@ -31,8 +32,7 @@
        		jr $ra			
        		
 		
-	printArr:
-		# void printArr(int arr[], int n)
+	printArr: # void printArr(int arr[], int n)
 		addi $sp, $sp, -8
 		# Guardando los valores de $s0 y $s1 en Stack para no modificar los valores originales
 		sw $s1, 0($sp) # indice del array
@@ -66,6 +66,90 @@
 			addi $sp, $sp, 8
 			
 			jr $ra
+		
+				
+	quickSortIterative: # void quickSortIterative (int arr[], int l, int h)
+		addi $sp, $sp, -16
+		# Guardando los valores de $s0 y $s1 en Stack para no modificar los valores originales
+		sw $s0, 0($sp) # Array
+		sw $s1, 4($sp) # indice del array: l
+		sw $s2, 8($sp) # Tamaño del array: h
+		sw $ra, 12($sp) # Dirección para regresar al main
+		sw $t0, stack
+		
+		sub $s3, $s2, $s1 # h - l
+		addi $s3, $s3, 1 # h - l + 1
+		
+		subi $s4, $zero, 1 # top = -1
+		addi $s4, $s4, 1 # top++
+		sll $t1, $s4, 2 # Multiplicando top * 4 para guardar en el stack
+		add $t2, $t1, $t0
+		add $t3, $zero, 1
+		sw $t3, 0($t2) # stack[too++] = 1
+		
+		addi $s4, $s4, 1 # top++
+		sll $t1, $s4, 2 # Multiplicando por 4
+		add $t2, $t1, $t0
+		sw $s2, 0($t2)
+		
+		while:
+			sge $s4, $zero, exit_while
+			subi $s4, $s4, 1 # top-- 
+			sll $t1, $s4, 2 # Multiplicando por 4
+			add $t2, $t1, $t0
+			lw $s2, 0($t2) # h = stack[ top-- ]
 			
-	quickSortIterative:
+			subi $s4, $s4, 1 # top--
+			sll $t1, $s4, 2 # Multiplicando por 4
+			add $t2, $t1, $t0
+			lw $s2, 0($t2)
+			
+			jal partition
+			
+			add $t4, $zero, $v1 # p = partition( arr, l, h)
+			subi $t5, $t4, 1 # p - 1
+			addi $t6, $t4, 1 # p + 1
+			
+			bgt $t5, $t3, else1 # if ( p -1 > 1)
+			addi $s4, $s4, 1 # top++
+			sll $t1, $s4, 2 # Multiplicando top * 4 para guardar en el stack
+			add $t2, $t1, $t0
+			sw $t3, 0($t2) # stack[++top] = 1
+			
+			addi $s4, $s4, 1 # top++
+			sll $t1, $s4, 2 # Multiplicando por 4
+			addi $t2, $t1, $t0
+			sw $t5, 0($t2) # stack[++top] = p-1
+			
+			else1:
+			
+			blt $t6, $s2, else2 # if ( p + 1 < h )
+			addi $s4, $s4, 1 # top++
+			sll $t1, $s4, 2 # Multiplicando top * 4 para guardar en el stack
+			add $t2, $t1, $t0
+			sw $t6, 0($t2) # stack[++top] = p + 1
+			
+			addi $s4, $s4, 1 # top++
+			sll $t1, $s4, 2 # Multiplicando por 4
+			addi $t2, $t1, $t0
+			sw $s2, 0($t2) # stack[++top] = h
+			
+			else2:
+			
+			j while
+			
+		exit_while:
+			# Regresando a los valores anteriores
+			lw $s0, 0($sp)
+			lw $s1, 4($sp)
+			lw $s2, 8($sp)
+			lw $ra, 12($sp) 
+			addi $sp, $sp, 8
+			
+			jr $ra
+			
+		
+		
+	partition:
+		# La parte de alics la asimetrix <3
 		
